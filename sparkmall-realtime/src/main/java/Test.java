89406252b4b267b8ffde7bd3ff536abbfbf4ba83
zxm1 <zxm1@qq.com>
2019-02-21 22:46:40 +0800

    修改了,增加了main方法

--- sparkmall-realtime/src/main/java/Test.java
+++ sparkmall-realtime/src/main/java/Test.java
@@ -1,9 +1,12 @@
+import java.util.Date;
+import java.util.List;
+
 /**
  * @author zxm
  * @create 2019/2/21 - 22:31
  */
 public class Test {
     public static void main(String[] args) {
-
+        new Date();
     }
 }
